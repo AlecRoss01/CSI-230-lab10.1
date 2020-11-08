0bdbba7e66fbc6c971a1262abf716946b93b1a8c
AlecRoss01 <61425095+AlecRoss01@users.noreply.github.com>
2020-11-08 14:50:19 -0500

    Create util.h

---
+++ util.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+//random element access
+string randomElement(vector<string> inputVector);
+
+//double functions
+double sum(vector<double> input);
+double avg(vector<double> input);
+double lowest(vector<double> input);
+
+//string manipulation
+void camelCase(string &input);
